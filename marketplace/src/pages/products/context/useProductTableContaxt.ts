4a8266e06f1f2590b9f1206bb7ac73280934c6c8
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-12-11 13:09:12 +0200

    comit

---
+++ marketplace/src/pages/products/context/useProductTableContaxt.ts
@@ -0,0 +1,20 @@
+/*import { useContext } from "react";
+import { ProductContext, ProductDispatchContext } from "../context/product.context"; // Змінити на ваш правильний шлях
+
+export const useProductContext = () => {
+  const context = useContext(ProductContext);
+  return context;
+};
+
+export const useProductDispatchContext = () => {
+  const context = useContext(ProductDispatchContext);
+
+  if (!context) {
+    throw new Error(
+      "useProductDispatchContext must be used within a ProductDispatchContextProvider"
+    );
+  }
+
+  return context;
+};*/
+
